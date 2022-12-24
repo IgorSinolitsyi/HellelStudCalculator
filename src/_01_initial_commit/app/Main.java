4f63ff4e350c9e0d7a94028b1b1f38f64c3f57eb
IgorSinolitsyi <ihor.sinolitsyi@gmail.com>
2022-12-24 20:59:25 +0100

    add input name

--- src/_01_initial_commit/app/Main.java
+++ src/_01_initial_commit/app/Main.java
@@ -1,8 +1,17 @@
 package _01_initial_commit.app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My git app.");
+      // System.out.println("My git app.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("My git app. is: ...? ");
+        System.out.print("Write the name:" );
+        String name = input.nextLine();
+        System.out.println("My git app. is: "+name);
+        input.close();
+
     }
 }
